120b6437c9f0cd44094c4029435f73b561e49cf4
Grace Masuku <gracekgalati@gmail.com>
2022-08-18 15:41:55 +0200

    Added files

---
+++ pizzaCart-api.js
@@ -0,0 +1,115 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('pizzaCartWithAPIWidget', function() {
+      return {
+            init() {
+                axios
+                    .get ('https://pizza-cart-api.herokuapp.com/api/pizzas')
+                    .then((result) => {  
+                    // console.log(result.data);
+                    this.pizzas = result.data.pizzas
+                     })
+                    .then(() => {
+                        return this.createCart();
+                    })
+                    // .then ((result) => {
+                    //     this.cartId = result.data.cart_code
+                    // });
+            },
+
+            createCart() {
+                return axios.get ('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username=' + this.username)
+                .then((result) => {  
+                // console.log(result.data);
+                this.cartId = result.data.cart_code
+                 })
+            },
+
+            showCart() {
+                // const cart_code = '...';
+                const url = `https://pizza-cart-api.herokuapp.com/api/pizza-cart/${this.cartId}/get`;
+
+                axios
+                .get(url)
+                .then((result) => {
+                    this.cart = result.data;
+                });
+            },
+
+            pizzaImage(pizza) {
+                return `./img/${pizza.size}.jpg`
+            },
+           
+            message : 'Eating pizzas',
+            username: 'Grace Masuku',
+            pizzas : [],
+            cartId: '',
+            cart: { total: 0},
+            // payNow: false,
+            paymentAmount: 0,
+            paymentMessage: '',
+
+            add(pizza) {
+                const params  = {
+                    cart_code: this.cartId,
+                    pizza_id : pizza.id
+                }
+            
+                axios 
+                    .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/add', params)
+                    .then(() => {
+
+                        this.message = "Pizzas added to the cart"
+                        this.showCart();
+                    })
+                    .catch(err => alert(err));
+            },
+            
+            remove(pizza) {
+                const params = {
+                  cart_code : this.cartId,
+                  pizza_id : pizza.id
+                }
+                axios
+                  .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/remove', params)
+                  .then(() => {
+                    this.message= "Pizzas removed from the cart"
+                    this.showCart();
+                  })
+                  .catch(err =>alert(err));
+              },
+        
+            pay() {
+                const params = {
+                  cart_code : this.cartId,
+                }
+                axios
+                  .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/pay', params)
+                  .then(() => {
+                      if(!this.paymentAmount) {
+                          this.paymentMessage = 'No amount entered!'
+                      }
+
+                      else if(this.paymentAmount >= this.cart.total.toFixed(2)){
+                          this.paymentMessage = 'Payment Sucessful!'
+                          this.message= this.username  + " Paid!"
+                          setTimeout(() => {
+                              this.cart.total = 0;
+                              this.paymentAmount = 0;
+                              this.paymentMessage = '';
+                              this.message = '';
+                          }, 3000);
+                      }else{
+                          this.paymentMessage = 'Sorry - You do not have enough money!'
+                          setTimeout(() => {
+                              this.cart.total = '';
+                              
+                          }, 3000);
+                      }
+                  })
+                  .catch(err=>alert(err));
+              },
+        }
+    });
+})
+        
+   
